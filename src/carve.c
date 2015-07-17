4962536d60364de0ea48d473493d325810a66b27
Victor Jiao <VjiaoBlack@gmail.com>
2015-07-16 22:37:42 -0400

    Energy function with log of gradient density (magnitude adjusted) works

--- src/carve.c
+++ src/carve.c
@@ -7,6 +7,80 @@ SDL_Window* gWindow = NULL;
 SDL_Surface* gScreenSurface = NULL;
 SDL_Surface* gPNGSurface = NULL;
 
+int find_energy( int r, int c, int rows, int cols, SDL_Surface* surface) {
+    Uint8 rl = 0, gl = 0, bl = 0,
+          rr = 0, gr = 0, br = 0,
+          rt = 0, gt = 0, bt = 0,
+          rb = 0, gb = 0, bb = 0;
+
+
+    Uint8 ro = 0, go = 0, bo = 0;
+    Uint32* pixels = (Uint32*) surface->pixels;
+
+    SDL_GetRGB( * (pixels + c  + r * cols ),
+            gScreenSurface->format, &ro, &go, &bo);
+
+    int num_sides = 0;
+
+    int asdf = 20;
+    int basdf = 21;
+    int energy = 0;
+
+
+    if (c > 0) {
+        SDL_GetRGB( pixels[c - 1 + r * cols] ,
+                gScreenSurface->format, &rl, &gl, &bl);
+        num_sides++;
+
+        // printf("before: %d energy\n", energy);
+        // printf("%d %d: %d %d %d to %d %d: %d %d %d\n", r, c, ro, go, bo, r, c-1, rl, gl, bl);
+        energy += abs((int)rl - (int)ro) + abs((int)gl - (int)go) + abs((int)bl - (int)bo);
+        // printf("after: %d energy\n", energy);
+
+    }
+
+    if (c < cols - 1) {
+        SDL_GetRGB( pixels[c + 1 + r * cols ],
+                gScreenSurface->format, &rr, &gr, &br);
+        num_sides++;
+
+        energy += abs((int)rr - (int)ro) + abs((int)gr - (int)go) + abs((int)br - (int)bo);
+
+    }
+
+    if (r > 0) {
+        SDL_GetRGB( pixels[c + (r - 1) * cols],
+                gScreenSurface->format, &rt, &gt, &bt);
+        num_sides++;
+
+        energy += abs((int)rt - (int)ro) + abs((int)gt - (int)go) + abs((int)bt - (int)bo);
+
+    }
+    if (r < rows - 1) {
+        SDL_GetRGB( pixels[c + (r + 1) * cols],
+                gScreenSurface->format, &rb, &gb, &bb);
+        num_sides++;
+
+        energy += abs((int)rb - (int)ro) + abs((int)gb - (int)go) + abs((int)bb - (int)bo);
+
+    }
+
+
+    energy /= 3;
+    energy /= num_sides;
+
+    double e = (double) energy;
+    e = log(e);
+
+    if (e < 0)
+        e = 0;
+
+    // 256 / ln(256)
+    e *= 46;
+
+    return (int) e;
+}
+
 int main( int argc, char* args[] ) {
 
     if (argc != 4) {
@@ -43,27 +117,23 @@ int main( int argc, char* args[] ) {
     char** energy = malloc(sizeof(char*) * rows);
     int i, j;
 
-    Uint8 ro, go, bo, r, g, b;
 
-
+    // avoid editing pixels and then re-putting them in same loop. Else, it breaks.
     for (i = 0; i < rows; i++) {
-        energy[i] = malloc(sizeof(char) * rows);
+        energy[i] = malloc(sizeof(char) * cols);
         for (j = 0; j < cols; j++) {
-            SDL_GetRGB( * (Uint32*)(gPNGSurface->pixels + j * sizeof(Uint32) + i * cols * sizeof(Uint32)),
-                gScreenSurface->format, &ro, &go, &bo);
-                *((Uint32*)gPNGSurface->pixels + j + i * cols) = SDL_MapRGB(
-                    gScreenSurface->format, ro* ((double)i / rows), go* ((double)i / rows), bo * ((double)i / rows));
+            energy[i][j] = (char) find_energy(i, j, rows, cols, gPNGSurface);
         }
     }
 
 
 
-
-
-
-
-    // analyze energy matrix
-
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            *((Uint32*)gPNGSurface->pixels + j + i * cols) = SDL_MapRGB(
+                    gScreenSurface->format, energy[i][j], energy[i][j], energy[i][j]);
+        }
+    }
 
 
 
